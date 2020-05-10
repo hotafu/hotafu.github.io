71bdcf8103f87d8ec974ff8503193ee74a49d046
hotafu <64695295+hotafu@users.noreply.github.com>
2020-05-10 21:27:25 +0700

    ok

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Hotafu.", "Designer.", "Developer."],
+      strings: ["Hotafu", "Designer", "Developer"],
       typeSpeed: 100,
       loop: true,
     });
