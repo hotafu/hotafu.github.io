d53764b1e8b6665204336d41b3a4872d7e44f445
hotafu <64695295+hotafu@users.noreply.github.com>
2020-05-10 11:58:04 +0700

    Update contactform.js

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -89,11 +89,20 @@ jQuery(document).ready(function($) {
       }
     });
     if (ferror) return false;
-    else var str = $(this).serialize();
+    else var str = { //gán các giá trị tương ứng vào các Input name tương ứng đã lấy ở trên
+        "emailAddress": email,
+        "entry.508196069": hoten,
+        "entry.5929986": tieude,
+        "entry.368243065": noidung
+    };
     var action = $(this).attr('action');
     if( ! action ) {
-      action = 'contactform/contactform.php';
+      action = 'https://docs.google.com/forms/d/e/1FAIpQLSeRmIc2JidpLcoiB0Ki-Erv3XlXU3ExZTAZacpN8K2qP2jVug/formResponse?';
     }
+    var email = $("#email").val();
+    var hoten = $("#hoten").val();
+    var tieude = $("#tieude").val();
+    var noidung = $("#noidung").val();
     $.ajax({
       type: "POST",
       url: action,
