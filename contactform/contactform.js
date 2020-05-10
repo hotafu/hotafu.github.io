2a5c21b9243157cd102904bd898aa5e66a300c60
hotafu <64695295+hotafu@users.noreply.github.com>
2020-05-10 12:39:22 +0700

    Update contactform.js

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -125,10 +125,10 @@ jQuery(document).ready(function($) {
       },
       error: function(x, y, z) {
           $('#sendmessage').show(); //hiện ra cái mess báo thành công khi gửi xong
-          $('#email').val("NO");
-          $('#hoten').val();
-          //$('#tieude').val();
-          //$('#noidung').val();
+          $('#email').val("");
+          $('#hoten').val("");
+          $('#tieude').val("");
+          $('#noidung').val("");
       }
     });
     return false;
